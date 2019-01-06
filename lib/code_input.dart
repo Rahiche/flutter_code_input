adbd20d1a6c7753e2699ebb714c1603bce2454c0
Rahiche <rraouf30@gmail.com>
2019-01-06 07:58:55 +0100

    add backgroundCursorColor *backgroundCursorColor is required because of [#25384](https://github.com/flutter/flutter/pull/25384) *fixed some typos *formatting

--- lib/code_input.dart
+++ lib/code_input.dart
@@ -7,9 +7,9 @@ import 'package:flutter/services.dart';
 
 /// A widget for inputting content with a fixed length, visually treating each
 /// character as a separate segment.
-/// 
+///
 /// ## Sample code
-/// 
+///
 /// ```dart
 /// CodeInput(
 ///   length: 4,
@@ -18,9 +18,9 @@ import 'package:flutter/services.dart';
 ///   onFilled: (value) => print('Your input is $value.'),
 /// )
 /// ```
-/// 
+///
 /// See also:
-/// 
+///
 /// * [TextField], an input where the characters aren't separated from each
 ///   other.
 typedef CodeInputBuilder = Widget Function(bool hasFocus, String char);
@@ -49,9 +49,8 @@ class CodeInput extends StatefulWidget {
     assert(length > 0, 'The length needs to be larger than zero.');
     assert(length.isFinite, 'The length needs to be finite.');
     assert(keyboardType != null);
-    assert(builder != null, 
-      'The builder is required for rendering the character segments.'
-    );
+    assert(builder != null,
+        'The builder is required for rendering the character segments.');
 
     inputFormatters ??= _createInputFormatters(length, keyboardType);
 
@@ -67,20 +66,20 @@ class CodeInput extends StatefulWidget {
   }
 
   /// The length of character entities to always display.
-  /// 
+  ///
   /// ## Sample code
-  /// 
+  ///
   /// A code input with 4 characters:
-  /// 
+  ///
   /// ```dart
   /// CodeInput(length: 4)
   /// ```
   final int length;
 
   /// The type of thconstard which shows up.
-  /// 
+  ///
   /// ## Sample codeconst
-  /// 
+  ///
   /// ```dart
   /// CodeInput(keyboardType: TextInputType.number)
   /// ```
@@ -88,15 +87,15 @@ class CodeInput extends StatefulWidget {
 
   /// A list of input formatters which can validate the text as it is being
   /// typed.
-  /// 
+  ///
   /// If you specify this parameter, the default input formatters aren't used,
   /// so make sure you really check for everything (like length of the input).
-  /// 
+  ///
   /// ## Sample code
-  /// 
+  ///
   /// An code input that displays a normal keyboard but only allows for
   /// hexadecimal input:
-  /// 
+  ///
   /// ```dart
   /// CodeInput(
   ///   inputFormatters: [
@@ -107,7 +106,7 @@ class CodeInput extends StatefulWidget {
   final List<TextInputFormatter> inputFormatters;
 
   /// A builder for the character entities.
-  /// 
+  ///
   /// See [CodeInputBuilders] for examples.
   final CodeInputBuilder builder;
 
@@ -117,13 +116,10 @@ class CodeInput extends StatefulWidget {
   /// A callback for when the input is filled.
   final void Function(String value) onFilled;
 
-
   /// A helping function that creates input formatters for a given length and
   /// keyboardType.
   static List<TextInputFormatter> _createInputFormatters(
-    int length,
-    TextInputType keyboardType
-  ) {
+      int length, TextInputType keyboardType) {
     final formatters = <TextInputFormatter>[
       LengthLimitingTextInputFormatter(length)
     ];
@@ -145,17 +141,17 @@ class CodeInput extends StatefulWidget {
 class _CodeInputState extends State<CodeInput> {
   final node = FocusNode();
   final controller = TextEditingController();
+
   String get text => controller.text;
 
   @override
   Widget build(BuildContext context) {
     // We'll display the visual widget and a not shown EditableText for doing
     // the actual work on top of each other.
-    return Stack(
-      children: <Widget>[
-        // This is the actual EditableText wrapped in a Container with zero
-        // dimensions.
-        Container(
+    return Stack(children: <Widget>[
+      // This is the actual EditableText wrapped in a Container with zero
+      // dimensions.
+      Container(
           width: 0.0,
           height: 0.0,
           child: EditableText(
@@ -163,20 +159,22 @@ class _CodeInputState extends State<CodeInput> {
             focusNode: node,
             inputFormatters: widget.inputFormatters,
             keyboardType: widget.keyboardType,
-            style: TextStyle(), // Doesn't really matter.
-            cursorColor: Colors.black, // Doesn't really matter.
+            backgroundCursorColor: Colors.black,
+            style: TextStyle(),
+            // Doesn't really matter.
+            cursorColor: Colors.black,
+            // Doesn't really matter.
             onChanged: (value) => setState(() {
-              widget.onChanged?.call(value);
-              if (value.length == widget.length) {
-                widget.onFilled?.call(value);
-              }
-            })
-          )
-        ),
-        // These are the actual character widgets. A transparent container lies
-        // right below the gesture detector, so all taps get collected, even
-        // the ones between the character entities.
-        GestureDetector(
+                  widget.onChanged?.call(value);
+                  if (value.length == widget.length) {
+                    widget.onFilled?.call(value);
+                  }
+                }),
+          )),
+      // These are the actual character widgets. A transparent container lies
+      // right below the gesture detector, so all taps get collected, even
+      // the ones between the character entities.
+      GestureDetector(
           onTap: () {
             final focusScope = FocusScope.of(context);
             focusScope.requestFocus(FocusNode());
@@ -191,36 +189,31 @@ class _CodeInputState extends State<CodeInput> {
                 final char = i < text.length ? text[i] : '';
                 final characterEntity = widget.builder(hasFocus, char);
 
-                assert(characterEntity != null,
-                  'The builder for the character entity at position $i '
-                  'returned null. It did${hasFocus ? ' not' : ''} have the '
-                  'focus and the character passed to it was \'$char\'.'
-                );
+                assert(
+                    characterEntity != null,
+                    'The builder for the character entity at position $i '
+                    'returned null. It did${hasFocus ? ' not' : ''} have the '
+                    'focus and the character passed to it was \'$char\'.');
 
                 return characterEntity;
               }),
             ),
-          )
-        ),
-      ]
-    );
+          )),
+    ]);
   }
 }
 
-
-
 /// An abstract class that provides some commonly-used builders for the
 /// character entities.
-/// 
+///
 /// * [containerized]: A builder putting chars in an animated container.
 /// * [circle]: A builder putting chars in circles.
 /// * [rectangle]: A builder putting chars in rectangles.
 /// * [lightCircle]: A builder putting chars in light circles.
 /// * [darkCircle]: A builder putting chars in dark circles.
-/// * [lightRectangle]: A builer putting chars in light rectangles.
-/// * [darkRectangle]: A builder putting chars in dark rectanlges.
+/// * [lightRectangle]: A builder putting chars in light rectangles.
+/// * [darkRectangle]: A builder putting chars in dark rectangles.
 abstract class CodeInputBuilders {
-
   /// Builds the input inside an animated container.
   static CodeInputBuilder containerized({
     Duration animationDuration = const Duration(milliseconds: 50),
@@ -233,31 +226,28 @@ abstract class CodeInputBuilders {
     @required TextStyle filledTextStyle,
   }) {
     return (bool hasFocus, String char) => Container(
-      width: totalSize.width,
-      height: totalSize.height,
-      alignment: Alignment.center,
-      child: AnimatedContainer(
-        duration: Duration(milliseconds: 100),
-        decoration: char.isEmpty ? emptyDecoration : filledDecoration,
-        width: char.isEmpty ? emptySize.width: filledSize.width,
-        height: char.isEmpty ? emptySize.height : filledSize.height,
+        width: totalSize.width,
+        height: totalSize.height,
         alignment: Alignment.center,
-        child: Text(char,
-          style: char.isEmpty ? emptyTextStyle : filledTextStyle
-        ),
-      )
-    );
+        child: AnimatedContainer(
+          duration: Duration(milliseconds: 100),
+          decoration: char.isEmpty ? emptyDecoration : filledDecoration,
+          width: char.isEmpty ? emptySize.width : filledSize.width,
+          height: char.isEmpty ? emptySize.height : filledSize.height,
+          alignment: Alignment.center,
+          child: Text(char,
+              style: char.isEmpty ? emptyTextStyle : filledTextStyle),
+        ));
   }
 
   /// Builds the input inside a circle.
-  static CodeInputBuilder circle({
-    double totalRadius = 30.0,
-    double emptyRadius = 10.0,
-    double filledRadius = 25.0,
-    @required Border border,
-    @required Color color,
-    @required TextStyle textStyle
-  }) {
+  static CodeInputBuilder circle(
+      {double totalRadius = 30.0,
+      double emptyRadius = 10.0,
+      double filledRadius = 25.0,
+      @required Border border,
+      @required Color color,
+      @required TextStyle textStyle}) {
     final decoration = BoxDecoration(
       shape: BoxShape.circle,
       border: border,
@@ -265,14 +255,13 @@ abstract class CodeInputBuilders {
     );
 
     return containerized(
-      totalSize: Size.fromRadius(totalRadius),
-      emptySize: Size.fromRadius(emptyRadius),
-      filledSize: Size.fromRadius(filledRadius),
-      emptyDecoration: decoration,
-      filledDecoration: decoration,
-      emptyTextStyle: textStyle.copyWith(fontSize: 0.0),
-      filledTextStyle: textStyle
-    );
+        totalSize: Size.fromRadius(totalRadius),
+        emptySize: Size.fromRadius(emptyRadius),
+        filledSize: Size.fromRadius(filledRadius),
+        emptyDecoration: decoration,
+        filledDecoration: decoration,
+        emptyTextStyle: textStyle.copyWith(fontSize: 0.0),
+        filledTextStyle: textStyle);
   }
 
   /// Builds the input inside a rectangle.
@@ -292,14 +281,13 @@ abstract class CodeInputBuilders {
     );
 
     return containerized(
-      totalSize: totalSize,
-      emptySize: emptySize,
-      filledSize: filledSize,
-      emptyDecoration: decoration,
-      filledDecoration: decoration,
-      emptyTextStyle: textStyle.copyWith(fontSize: 0.0),
-      filledTextStyle: textStyle
-    );
+        totalSize: totalSize,
+        emptySize: emptySize,
+        filledSize: filledSize,
+        emptyDecoration: decoration,
+        filledDecoration: decoration,
+        emptyTextStyle: textStyle.copyWith(fontSize: 0.0),
+        filledTextStyle: textStyle);
   }
 
   /// Builds the input inside a light circle.
@@ -309,17 +297,13 @@ abstract class CodeInputBuilders {
     double filledRadius = 25.0,
   }) {
     return circle(
-      totalRadius: totalRadius,
-      emptyRadius: emptyRadius,
-      filledRadius: filledRadius,
-      border: Border.all(color: Colors.white, width: 2.0),
-      color: Colors.white10,
-      textStyle: TextStyle(
-        color: Colors.white,
-        fontSize: 20.0,
-        fontWeight: FontWeight.bold
-      )
-    );
+        totalRadius: totalRadius,
+        emptyRadius: emptyRadius,
+        filledRadius: filledRadius,
+        border: Border.all(color: Colors.white, width: 2.0),
+        color: Colors.white10,
+        textStyle: TextStyle(
+            color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold));
   }
 
   /// Builds the input inside a light circle.
@@ -329,17 +313,13 @@ abstract class CodeInputBuilders {
     double filledRadius = 25.0,
   }) {
     return circle(
-      totalRadius: totalRadius,
-      emptyRadius: emptyRadius,
-      filledRadius: filledRadius,
-      border: Border.all(color: Colors.black, width: 2.0),
-      color: Colors.black12,
-      textStyle: TextStyle(
-        color: Colors.black,
-        fontSize: 20.0,
-        fontWeight: FontWeight.bold
-      )
-    );
+        totalRadius: totalRadius,
+        emptyRadius: emptyRadius,
+        filledRadius: filledRadius,
+        border: Border.all(color: Colors.black, width: 2.0),
+        color: Colors.black12,
+        textStyle: TextStyle(
+            color: Colors.black, fontSize: 20.0, fontWeight: FontWeight.bold));
   }
 
   /// Builds the input inside a light rectangle.
@@ -350,18 +330,14 @@ abstract class CodeInputBuilders {
     BorderRadius borderRadius = BorderRadius.zero,
   }) {
     return rectangle(
-      totalSize: totalSize,
-      emptySize: emptySize,
-      filledSize: filledSize,
-      borderRadius: borderRadius,
-      border: Border.all(color: Colors.white, width: 2.0),
-      color: Colors.white10,
-      textStyle: TextStyle(
-        color: Colors.white,
-        fontSize: 20.0,
-        fontWeight: FontWeight.bold
-      )
-    );
+        totalSize: totalSize,
+        emptySize: emptySize,
+        filledSize: filledSize,
+        borderRadius: borderRadius,
+        border: Border.all(color: Colors.white, width: 2.0),
+        color: Colors.white10,
+        textStyle: TextStyle(
+            color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold));
   }
 
   /// Builds the input inside a dark rectangle.
@@ -372,18 +348,13 @@ abstract class CodeInputBuilders {
     BorderRadius borderRadius = BorderRadius.zero,
   }) {
     return rectangle(
-      totalSize: totalSize,
-      emptySize: emptySize,
-      filledSize: filledSize,
-      borderRadius: borderRadius,
-      border: Border.all(color: Colors.black, width: 2.0),
-      color: Colors.black12,
-      textStyle: TextStyle(
-        color: Colors.black,
-        fontSize: 20.0,
-        fontWeight: FontWeight.bold
-      )
-    );
+        totalSize: totalSize,
+        emptySize: emptySize,
+        filledSize: filledSize,
+        borderRadius: borderRadius,
+        border: Border.all(color: Colors.black, width: 2.0),
+        color: Colors.black12,
+        textStyle: TextStyle(
+            color: Colors.black, fontSize: 20.0, fontWeight: FontWeight.bold));
   }
 }
-
